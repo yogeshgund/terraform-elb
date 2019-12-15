87d7669961c51201b84e79fdf45793abe57820f2
yogeshgund <54471844+yogeshgund@users.noreply.github.com>
2019-12-15 17:23:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-central-1"
-  access_key = "***********************"
-  secret_key = "***********************"
+  region     = "us-east-1"
+  access_key = "AKIA3LP4FB7Z76KQNSFK"
+  secret_key = "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"
 }
